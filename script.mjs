61f345bb2bde384eec0a1da2ce81cec11ee418bd
Najeeb Ullah <najeebullah9050@gmail.com>
2023-10-29 05:34:32 -0700

    reform

--- script.mjs
+++ script.mjs
@@ -1,15 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const celsiusInput = document.getElementById("celsius");
-    const fahrenheitOutput = document.getElementById("fahrenheit");
-    const convertButton = document.getElementById("convertButton");
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
 
-    convertButton.addEventListener("click", function () {
-        const celsius = parseFloat(celsiusInput.value);
-        if (!isNaN(celsius)) {
-            const fahrenheit = (celsius * 9/5) + 32;
-            fahrenheitOutput.textContent = fahrenheit.toFixed(2);
-        } else {
-            fahrenheitOutput.textContent = "Invalid input";
-        }
-    });
-});
+function celToFar() {
+    let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function farToCel() {
+    let output = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+}
